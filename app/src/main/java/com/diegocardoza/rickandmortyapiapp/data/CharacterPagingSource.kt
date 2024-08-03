614de450c1369bc0a901b610344c7bf66b719d14
Diego Cardoza <diegoalfredocar@gmail.com>
2024-08-03 15:33:59 -0600

    Add main list with Paging3

--- app/src/main/java/com/diegocardoza/rickandmortyapiapp/data/CharacterPagingSource.kt
+++ app/src/main/java/com/diegocardoza/rickandmortyapiapp/data/CharacterPagingSource.kt
@@ -1,6 +1,5 @@
 package com.diegocardoza.rickandmortyapiapp.data
 
-import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.diegocardoza.rickandmortyapiapp.presentation.model.CharacterModel
